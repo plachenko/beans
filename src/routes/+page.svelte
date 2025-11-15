574da1f3bbb7d4c74cbc329641ccfece23b33160
Denis Perchenko <plachenko@gmail.com>
2025-11-14 23:52:22 -0500

    basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+<div class="bg-red-400 flex gap-2 flex-col items-center justify-center h-screen w-screen">
+    <h1 class="header text-4xl font-bold">Beans dot com</h1>
+    <h2>Find a your next man's best friend friend</h2>
+    <div class="bg-blue-400 h-1/2 w-1/2 p-5">
+        <div class="bg-black w-full h-full"></div>
+    </div>
+    <div class="p-2 bg-green-300 w-1/2 flex flex-col items-center justify-center">
+        <button class="bg-white p-2 rounded-md border-2 cursor-pointer">find friend</button>
+    </div>
+</div>
